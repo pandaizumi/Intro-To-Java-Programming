2ee95ebfa8023938c02b6d380bbccb0fe61637f4
XL'anna Brooks <izumichan31@gmail.com>
2021-01-11 21:28:08 -0500

    Programming Exercise 1.2

---
+++ src/chapter01/Exercise1_02.java
@@ -0,0 +1,13 @@
+// Display "Welcome to Java" 5 times
+
+package chapter01;
+
+public class Exercise1_02 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+    }
+}
